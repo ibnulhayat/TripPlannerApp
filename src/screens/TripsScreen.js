86e9754019f5ad01084c8ff0be20db86df2fbcb2
Ibnul Hayat <53576655+ibnulhayat@users.noreply.github.com>
2025-07-15 20:12:24 +0600

    show all created trip list in my trip screen

--- src/screens/TripsScreen.js
+++ src/screens/TripsScreen.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import {
+  FlatList,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import { useSelector } from 'react-redux';
+import AppToolBar from '../components/AppToolBar';
+import { COLORS } from '../styles/colors';
+import moment from 'moment';
 
 export default function TripsScreen() {
-  
+  const trips = useSelector((state) => state.trip.trips);
+
+  const renderTrip = ({ item }) => (
+    <View style={styles.card}>
+      <View style={{flex: 1}}>
+        <Text style={styles.tripText}>
+          {item.loadLocation} ➜ {item.unloadLocation}
+        </Text>
+        <Text style={styles.dateText}>{moment(new Date(item.date)).format('MMM DD, YYYY . h:mm A')}</Text>
+      </View>
+      <Image source={require('../assets/image/trip.png')} style={styles.imageStyle}/>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Trip</Text>
+      <AppToolBar
+        title={'Your Trips'}
+      />
+      <FlatList
+        data={trips}
+        keyExtractor={(item) => item.id}
+        renderItem={renderTrip}
+        ListEmptyComponent={<Text style={styles.emptyText}>No trips available.</Text>}
+      />
     </View>
   );
 }
@@ -18,11 +45,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.appBackgroundColor,
   },
-  title: {
-    fontSize: 22,
-    fontWeight: '700',
-    marginBottom: 16,
-  }
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 12
+  },
+  tripText: {
+    fontSize: 14,
+    color: COLORS.placeholderTextColor
+  },
+  dateText: {
+    fontSize: 16,
+    color: COLORS.titleTextColor,
+    fontWeight: 'bold',
+    marginTop: 4,
+  },
+  imageStyle:{
+    width: 130,
+    height: 65,
+    resizeMode: 'contain'
+  },
+  emptyText: {
+    marginTop: 50,
+    textAlign: 'center',
+    color: COLORS.titleTextColor,
+    fontSize: 16,
+  },
 });
